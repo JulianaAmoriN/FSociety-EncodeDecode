f1861b7da51758c5682ac0421943d8d565a1a77f
JulianaAmoriN <juamorim.sn@gmail.com>
2020-11-02 20:24:43 -0300

    separei o arquivo js e inseri o loop for(erro undefined)

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
 const cipher = {
-  // ...
+  encode : function(numero11,capturando11){
+
+    let texto="";
+
+    for (let index = 0; index < capturando11.length; index++) {
+    
+      let resposta1 = ((capturando11.charCodeAt(index) - 65) + numero11) % 26 + 65;
+      let codificado1 = String.fromCharCode(resposta1);
+
+      texto += codificado1[index];   
+    }
+    
+    return texto
+
+  }
+  , 
+  decode : function(numero22,capturando22){
+
+    let texto2="";
+
+    for (let index = 0; index < capturando22.length; index++) {
+
+      let resposta2 = ((capturando22.charCodeAt(0) - 90) - numero22) % 26 + 90;
+      let codificando2 =  String.fromCharCode(resposta2)
+      
+      texto2 += codificando2[index];   
+    }
+
+    return texto2
+
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-
 let capturando1 = "";
 let capturando11 = "";
 let numero1 = "";
@@ -15,8 +14,6 @@ let numero2 ="";
 const botao2 = document.getElementById("send2")
 botao2.addEventListener("click",capturar2)
 
-
-
 function capturar1 (event) {
     event.preventDefault()
     numero1 = document.getElementById("scret1").value;
@@ -26,17 +23,10 @@ function capturar1 (event) {
     capturando1 = document.getElementById('message1').value;
     capturando11 = capturando1.toUpperCase()
 
-    let resposta1 = ((capturando11.charCodeAt(0) - 65) + numero11) % 25 + 65;
-    let codificado1 = String.fromCharCode(resposta1)
 
-    document.getElementById('answer1').innerHTML = codificado1;
 
-    console.log(resposta1)
-    console.log(capturando1)
-    console.log(capturando11)
-    console.log(numero1)
-    console.log(numero11)
-    console.log(codificado1)
+    document.getElementById('answer1').innerHTML = cipher.encode(numero11,capturando11);
+
 }
 
 function capturar2 (event) {
@@ -48,24 +38,10 @@ function capturar2 (event) {
     capturando2 = document.getElementById('message2').value;
     capturando22 = capturando2.toUpperCase()
 
-    let resposta2 = ((capturando22.charCodeAt(0) - 65) - numero22) % 25 + 65;
-    let codificando2 =  String.fromCharCode(resposta2)
-
-    document.getElementById('answer2').innerHTML = codificando2;
+   
+   
+    document.getElementById('answer2').innerHTML = cipher.decode(numero22,capturando22);
     
-    console.log(capturando2)
-    console.log(capturando22)
-    console.log(numero2)
-    console.log(numero22)
-    console.log(resposta2)
-    console.log(codificando2)
+
 }
 
-
-
-//lincar meu elemnto secret com variavel e depois a função com o offset dessa forma ele vai enviar a mensagem pro deslocamento 
-
-// criar uma 
-//const =secret 
-//function {
-    //
