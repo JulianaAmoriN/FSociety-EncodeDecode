41aad49a78e46afaa743bc89f23e09aca2827e8c
JulianaAmoriN <juamorim.sn@gmail.com>
2020-11-03 11:49:49 -0300

    codigo funciona js

--- cipher.js
+++ cipher.js
@@ -1,17 +1,24 @@
 const cipher = {
+  //declaro dentro do obejto da variavel a função, sendo assim o que nomeia a função é o objeto que ela esta dentro 
   encode : function(numero11,capturando11){
 
     let texto="";
 
     for (let index = 0; index < capturando11.length; index++) {
+      //index é o parametro da posição inicial  de onde deve começar o loop 
+      //enquanto index for menor que o numero retornado por length deve continuar o loop (parametro)
+      //adiciona 1 a index pro processo ser iniciado novamente (incremento)
     
       let resposta1 = ((capturando11.charCodeAt(index) - 65) + numero11) % 26 + 65;
       let codificado1 = String.fromCharCode(resposta1);
 
-      texto += codificado1[index];   
+      texto += codificado1;   
+      console.log(codificado1)
     }
     
     return texto
+    //return finaliza a execução de uma função e 
+    //especifica os valores que devem ser retonados para onde a função foi chamada
 
   }
   , 
@@ -21,10 +28,11 @@ const cipher = {
 
     for (let index = 0; index < capturando22.length; index++) {
 
-      let resposta2 = ((capturando22.charCodeAt(0) - 90) - numero22) % 26 + 90;
+      let resposta2 = ((capturando22.charCodeAt(index) - 90) - numero22) % 26 + 90;
       let codificando2 =  String.fromCharCode(resposta2)
       
-      texto2 += codificando2[index];   
+      texto2 += codificando2;  
+      console.log(codificando2) 
     }
 
     return texto2
