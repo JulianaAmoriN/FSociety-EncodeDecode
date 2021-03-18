ef62cdbc514a38ad1414ede7690019a137f7f612
JulianaAmoriN <juamorim.sn@gmail.com>
2021-03-18 15:45:53 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -11,32 +11,25 @@ copyButton.addEventListener("click",(copy))
     
 function copy (event){
     event.preventDefault();
-
     const textInput = document.getElementById("answerEncode");
-
     textInput.select();
     document.execCommand("copy");
 }
+
 function formEncode (event) {
     event.preventDefault();
-    
     const numberEncode = document.getElementById("scretEncode").value;
     const offSetEncode = parseInt(numberEncode);
-
     const printEncode = document.getElementById("messageEncode").value;
     const textEncode = printEncode.toUpperCase();
-
     document.getElementById("answerEncode").innerHTML = cipher.encode(offSetEncode,textEncode);
 }
+
 function formDecode (event) {
     event.preventDefault();
-
     const numberDecode =document.getElementById("scretDecode").value;
     const offSetDecode = parseInt(numberDecode);
-
     const printDecode = document.getElementById("messageDecode").value;
     const textDecode = printDecode.toUpperCase();
-   
     document.getElementById("answerDecode").innerHTML = cipher.decode(offSetDecode,textDecode);
 }
-
